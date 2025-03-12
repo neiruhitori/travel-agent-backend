92a490290d1aa3a134793771a3d398602d3785dc
bimocahyo7 <bimochayo@gmail.com>
2025-03-12 16:14:30 +0700

    add API routes, model, and controller for User

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,9 +15,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
+            $table->enum('role', ['admin', 'customer'])->default('customer');
             $table->timestamps();
         });
 
