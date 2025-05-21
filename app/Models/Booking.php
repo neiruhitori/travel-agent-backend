f040222ae4d3e5f5753eadd9271397c5fc8fa2f1
Wahyudinataa <wahyudinata336@gmail.com>
2025-05-21 14:01:10 +0700

    revisi pada booking controller

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
+
 class Booking extends Model
 {
     use HasFactory;
